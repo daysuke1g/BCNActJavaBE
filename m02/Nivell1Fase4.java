1b8d23b13e922e84f7280ef4ab2e4f95c1a80980
daysuke1g <daysuke1g@gmail.com>
2021-03-27 15:15:49 +0100

    Commit for m02 module

--- m02/Nivell1Fase4.java
+++ m02/Nivell1Fase4.java
@@ -10,11 +10,9 @@ public class Nivell1Fase4
 	  A més, afegeix una posició amb un espai buit entre la primera i la segona. 
 	    És a dir, partim de la llista name i surname i al acabar l’execució només tindrem una que es dirà fullName. 
      */	        	  
-
 	 char[] name    = { 'J','o','s','é', 'L','u','i','s' } ; // o .. "".toCharArray(); 
      char[] surname	= { 'C','a','ñ','i','v','a','n','o' } ;    
-     
-	  
+     	 
      char[] fullName = new char[name.length+1+surname.length]; //init dest buffer 
      
      int dPosNomComplet=0;
